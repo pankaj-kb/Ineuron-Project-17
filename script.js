455be3568855f6b8b98cc2c8779b47c01422cd28
pankaj-kb <pankajb2601@gmail.com>
2023-02-08 21:14:48 +0530

    task 1 completed

---
+++ script.js
@@ -0,0 +1,14 @@
+// Task 1
+let barbarianColor = document.querySelector(".clearfix")
+barbarianColor.style.backgroundColor = "#EC9B3B"
+
+statColor = document.querySelectorAll(".stat")
+statValueColor = document.querySelectorAll(".stat-value")
+
+for (let element of statColor) {
+    element.style.color = "#FFFFFF";
+}
+
+for (let element of statValueColor) {
+    element.style.color = "#FFFFFF";
+}
